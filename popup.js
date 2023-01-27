6e1f36c7328ce5c2756d9cbbf3cd7b7a9e5ef092
JoKeR-VIKING <prathamvasani1@gmail.com>
2023-01-27 22:34:07 +0530

    popup close issue

--- popup.js
+++ popup.js
@@ -2,6 +2,6 @@ let popup = document.getElementById('popup');
 let close = document.getElementsByClassName('popup__close')[0];
 
 document.addEventListener('mouseup', (event) => {
-    if (!event.target.closest('.popup__content'))
+    if (window.getComputedStyle(popup).visibility !== "hidden" && !event.target.closest('.popup__content'))
         close.click();
 });
